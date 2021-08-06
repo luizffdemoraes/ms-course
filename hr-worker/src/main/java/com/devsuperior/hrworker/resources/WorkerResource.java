49c78170c8f54c014570c1a96d19a3d86d4c4fb1
luizffdemoraes <lffm1994@gmail.com>
2021-08-05 23:41:23 -0300

    Configuring Eureka clients

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -35,6 +35,8 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker>  findById(@PathVariable Long id){
 
+
+
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
         Worker obj = repository.findById(id).get();
